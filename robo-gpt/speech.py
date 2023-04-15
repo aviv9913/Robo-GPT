5d77c06df398d9be92b2928ca27417fbaf5fba02
Aviv <aviv.shitrit@campus.technion.ac.il>
2023-04-15 14:18:49 +0300

    Implementing speechT5 class

--- robo-gpt/speech.py
+++ robo-gpt/speech.py
@@ -4,6 +4,13 @@ from threading import Thread
 import requests
 from playsound import playsound
 
+from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
+import numpy as np
+import torch
+import soundfile as sf
+import simpleaudio as sa
+
+
 
 BASE_URL = "https://api.elevenlabs.io/v1/text-to-speech"
 VOICE_ID = "ErXwobaYiN019PkySvjV"
@@ -26,3 +33,30 @@ def say(text: str) -> None:
             playsound(temp.name)
     else:
         print(f"Failed to speak: status code = {response.status_code}\n{response.content}")
+
+
+class SpeechT5Speaker:
+    def __init__(self):
+        self.processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
+        self.model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
+        self.vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
+
+        speaker_embedding_array = np.load(r"C:\repos\Robo-GPT\robo-gpt\cmu_us_slt_arctic-wav-arctic_a0508.npy")
+        self.speaker_embedding = torch.from_numpy(speaker_embedding_array).unsqueeze(0)
+
+    def speak(self, text:str):
+        tempfile_name = "speech_tmp.wav"
+        inputs = self.processor(text=text, return_tensors="pt")
+        speech = self.model.generate_speech(inputs["input_ids"], self.speaker_embedding, vocoder=self.vocoder)
+        sf.write(tempfile_name, speech.numpy(), samplerate=16000)
+        playsound(tempfile_name)
+        try:
+            os.remove(tempfile_name)
+        except:
+            pass
+
+
+if __name__ == "__main__":
+    speaker = SpeechT5Speaker()
+    speaker.speak("Hello, my cat name is 'Sherry'")
+    speaker.speak("And i'm your personal assistant")
